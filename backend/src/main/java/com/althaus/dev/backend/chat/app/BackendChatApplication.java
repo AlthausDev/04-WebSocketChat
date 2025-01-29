4bae1361bfbdf25b991faab173b6398e39783560
AlthausDev <samuelalthaus@gmail.com>
2025-01-29 19:08:46 +0100

    feat: added chat component

--- backend/src/main/java/com/althaus/dev/backend/chat/app/BackendChatApplication.java
+++ backend/src/main/java/com/althaus/dev/backend/chat/app/BackendChatApplication.java
@@ -9,5 +9,4 @@ public class BackendChatApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendChatApplication.class, args);
 	}
-
 }
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ChatComponent } from "./components/chat.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ChatComponent],
   templateUrl: './app.component.html' 
 })
+
 export class AppComponent { 
 }
