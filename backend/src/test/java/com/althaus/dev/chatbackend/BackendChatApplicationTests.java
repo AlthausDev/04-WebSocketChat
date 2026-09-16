d076c96e1a4335c64c129e4aaa7247ebfb462ddd
S.Althaus <samuelalthaus@gmail.com>
2026-09-16 22:28:12 +0200

    test: align application test with root package

---
+++ backend/src/test/java/com/althaus/dev/chatbackend/BackendChatApplicationTests.java
@@ -0,0 +1,12 @@
+package com.althaus.dev.chatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
